54f7e20af133052f20defe7e7ee195f59f340804
Jennifer Sence <sencejenni@gmail.com>
2023-09-06 16:29:21 +0100

    created the variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const apikey = "2b2a6c7e6de0d339066ce5917cd90ab5";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
