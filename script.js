70ede517b7b8da0fa7948606d03667779e520fa9
Jennifer Sence <sencejenni@gmail.com>
2023-09-06 16:35:06 +0100

    started event listener for the formEl

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
 
 const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const cityValue = cityInputEl.value;
+    getWeatherData(cityValue);
+})
+
+function getWeatherData(cityValue){
+    try {
+        
+    } catch (error) {
+        
+    }
+}
